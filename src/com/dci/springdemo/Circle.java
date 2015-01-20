1105f4909476416f1818bfd355a4b61e531233ee
Luke Tornquist <ltornquist@deloitte.com>
2015-01-20 10:56:32 -0500

    Add changes for video 21

--- src/com/dci/springdemo/Circle.java
+++ src/com/dci/springdemo/Circle.java
@@ -1,5 +1,10 @@
 package com.dci.springdemo;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
@@ -7,6 +12,7 @@ import javax.annotation.Resource;
 /**
  * Created by ltornquist on 1/20/2015.
  */
+@Controller
 public class Circle implements Shape {
 
 	private Point center;

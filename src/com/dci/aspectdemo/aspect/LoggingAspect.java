c111f78c6c449124f54de9d0782d898b47e0a76d
Luke Tornquist <ltornquist@deloitte.com>
2015-01-20 14:50:18 -0500

    Add changes for video 29

--- src/com/dci/aspectdemo/aspect/LoggingAspect.java
+++ src/com/dci/aspectdemo/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.dci.aspectdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by ltornquist on 1/20/2015.
@@ -9,9 +10,18 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void LoggingAdvice()
 	{
 		System.out.println("Advice run. Get Method called");
 	}
+
+	@Before("allGetters()")
+	public void secondAdvice()
+	{
+		System.out.println("Second Advice executed.");
+	}
+
+	@Pointcut("execution(* get*())")
+	public void allGetters(){}
 }

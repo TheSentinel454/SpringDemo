cfb630559019dec5ed4511c41acef61b29339f5b
Luke Tornquist <ltornquist@deloitte.com>
2015-01-20 07:14:58 -0500

    Add the initial project

---
+++ src/com/dci/springdemo/DrawingApp.java
@@ -0,0 +1,20 @@
+package com.dci.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ltornquist on 1/16/2015.
+ */
+public class DrawingApp {
+
+	public static void main(String[] args)
+	{
+		//Triangle triangle = new Triangle();
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+}
---
+++ src/com/dci/springdemo/Triangle.java
@@ -0,0 +1,12 @@
+package com.dci.springdemo;
+
+/**
+ * Created by ltornquist on 1/16/2015.
+ */
+public class Triangle {
+
+	public void draw()
+	{
+		System.out.println("Triangle drawn!");
+	}
+}

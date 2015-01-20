c69739117f2b7beef428004d579e25949c556551
Luke Tornquist <ltornquist@deloitte.com>
2015-01-20 08:11:41 -0500

    Changes for video 8

---
+++ src/com/dci/springdemo/Point.java
@@ -0,0 +1,26 @@
+package com.dci.springdemo;
+
+/**
+ * Created by ltornquist on 1/20/2015.
+ */
+public class Point {
+
+	private int x;
+	private int y;
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+}
--- src/com/dci/springdemo/Triangle.java
+++ src/com/dci/springdemo/Triangle.java
@@ -5,8 +5,38 @@ package com.dci.springdemo;
  */
 public class Triangle {
 
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 	public void draw()
 	{
-		System.out.println("Triangle drawn!");
+		System.out.println( "Point A = (" + getPointA().getX() + "," + getPointA().getY() + ")\n" +
+							"Point B = (" + getPointB().getX() + "," + getPointB().getY() + ")\n" +
+							"Point C = (" + getPointC().getX() + "," + getPointC().getY() + ")");
 	}
 }

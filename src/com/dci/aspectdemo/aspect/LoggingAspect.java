053d593e6dc60e3ad3b082da91a008f3e508a6ab
Luke Tornquist <ltornquist@deloitte.com>
2015-01-21 08:23:07 -0500

    Add changes for video 35

--- src/com/dci/aspectdemo/aspect/LoggingAspect.java
+++ src/com/dci/aspectdemo/aspect/LoggingAspect.java
@@ -1,39 +1,14 @@
 package com.dci.aspectdemo.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+//import org.aspectj.lang.annotation.*;
 
 /**
  * Created by ltornquist on 1/20/2015.
  */
-@Aspect
 public class LoggingAspect {
 
-	@Before("allCircleMethods()")
-	public void LoggingAdvice(JoinPoint joinPoint)
-	{
-		//Circle circle = (Circle) joinPoint.getTarget();
-
-		//System.out.println(joinPoint.getTarget().toString());
-		//System.out.println("Advice run. Get Method called");
-	}
-
-	@AfterReturning(pointcut = "args(argument)", returning = "returnString")
-	public void stringArgumentMethods(String argument, Object returnString)
-	{
-		System.out.println("A method that takes String arguments has been called.");
-		System.out.println("Value: " + argument + " Output: " + returnString);
-	}
-
-	@AfterThrowing(pointcut = "args(argument)", throwing = "ex")
-	public void exceptionAdvice(String argument, Exception ex)
-	{
-		System.out.println("An exception has been thrown.");
-		System.out.println("Arg: " + argument + " Exception: " + ex);
-	}
-
-	@Around("@annotation(com.dci.aspectdemo.aspect.Loggable)")
+	//@Around("allGetters()")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint)
 	{
 		Object returnValue = null;
@@ -48,15 +23,6 @@ public class LoggingAspect {
 		return returnValue;
 	}
 
-	@Pointcut("execution(* get*())")
-	public void allGetters(){}
-
-	//@Pointcut("execution(* com.dci.aspectdemo.service.*Service.*(..))")
-	//public void allServices(){}
-
-	//@Pointcut("execution(* com.dci.aspectdemo.model.Circle.*(..))")
-	@Pointcut("within(com.dci.aspectdemo.model.Circle)")
-	public void allCircleMethods(){}
-
-
+	//@Pointcut("execution(* get*())")
+	//public void allGetters(){}
 }
